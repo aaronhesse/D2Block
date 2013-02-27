877e432781dee514b0339fb85abbf2aec2380503
ahesse <aaron.hesse@scourcritical.com>
2013-02-26 19:37:49 -0800

    Made some pointers const. Made signal and slot arguments const references.

--- Source/D2Block/src/main.cpp
+++ Source/D2Block/src/main.cpp
@@ -5,8 +5,8 @@
 #include "D2BlockLogitechLCD.h"
 #include <QtConcurrentRun>
 
-static void updaterWorkerThreadFunc(D2BlockUpdater*);
-static void lcdHandlerWorkerThreadFunc(D2BlockLogitechLCD*);
+static void updaterWorkerThreadFunc(D2BlockUpdater* const);
+static void lcdHandlerWorkerThreadFunc(D2BlockLogitechLCD* const);
 
 int main(int argc, char *argv[])
 {
@@ -15,22 +15,22 @@ int main(int argc, char *argv[])
     D2BlockUpdater updater;
     D2BlockWindow window;
 
-    QObject::connect(&app,     SIGNAL(setProgressBar(qint32)),            &window,        SLOT(on_setProgressBar(qint32)));
-    QObject::connect(&app,     SIGNAL(setProgressTitle(QString)),         &window,        SLOT(on_setProgressTitle(QString)));
-    QObject::connect(&app,     SIGNAL(setFilePathText(QString)),          &window,        SLOT(on_setFilePathText(QString)));
-	QObject::connect(&app,     SIGNAL(setProgressBar(qint32)),            &lcdHandler,    SLOT(on_setProgressBar(qint32)));
-	QObject::connect(&app,     SIGNAL(setProgressTitle(QString)),         &lcdHandler,    SLOT(on_setProgressTitle(QString)));
+    QObject::connect(&app,     SIGNAL(setProgressBar(const qint32&)),      &window,        SLOT(on_setProgressBar(const qint32&)));
+    QObject::connect(&app,     SIGNAL(setProgressTitle(const QString&)),   &window,        SLOT(on_setProgressTitle(const QString&)));
+    QObject::connect(&app,     SIGNAL(setFilePathText(const QString)),     &window,        SLOT(on_setFilePathText(const QString&)));
+	QObject::connect(&app,     SIGNAL(setProgressBar(const qint32&)),      &lcdHandler,    SLOT(on_setProgressBar(const qint32&)));
+	QObject::connect(&app,     SIGNAL(setProgressTitle(const QString&)),   &lcdHandler,    SLOT(on_setProgressTitle(const QString&)));
 
-    QObject::connect(&updater, SIGNAL(updaterComplete()),                 &app,           SLOT(on_updateComplete()));
-    QObject::connect(&updater, SIGNAL(showProgressBar()),                 &window,        SLOT(on_showProgressBar()));
-    QObject::connect(&updater, SIGNAL(updateProgressBar(qint32)),         &window,        SLOT(on_updateProgressBar(qint32)));
-    QObject::connect(&updater, SIGNAL(setProgressTitle(QString)),         &window,        SLOT(on_setProgressTitle(QString)));
-    QObject::connect(&updater, SIGNAL(setFilePathText(QString)),          &window,        SLOT(on_setFilePathText(QString)));
-    QObject::connect(&updater, SIGNAL(diablo2NotInstalled()),             &window,        SLOT(on_diablo2NotInstalled()));
-	QObject::connect(&updater, SIGNAL(updateProgressBar(qint32)),         &lcdHandler,    SLOT(on_updateProgressBar(qint32)));
-	QObject::connect(&updater, SIGNAL(setProgressTitle(QString)),         &lcdHandler,    SLOT(on_setProgressTitle(QString)));
+    QObject::connect(&updater, SIGNAL(updaterComplete()),                  &app,           SLOT(on_updateComplete()));
+    QObject::connect(&updater, SIGNAL(showProgressBar()),                  &window,        SLOT(on_showProgressBar()));
+    QObject::connect(&updater, SIGNAL(updateProgressBar(const qint32&)),   &window,        SLOT(on_updateProgressBar(const qint32&)));
+    QObject::connect(&updater, SIGNAL(setProgressTitle(const QString&)),   &window,        SLOT(on_setProgressTitle(const QString&)));
+    QObject::connect(&updater, SIGNAL(setFilePathText(const QString&)),    &window,        SLOT(on_setFilePathText(const QString&)));
+    QObject::connect(&updater, SIGNAL(diablo2NotInstalled()),              &window,        SLOT(on_diablo2NotInstalled()));
+	QObject::connect(&updater, SIGNAL(updateProgressBar(const qint32&)),   &lcdHandler,    SLOT(on_updateProgressBar(const qint32&)));
+	QObject::connect(&updater, SIGNAL(setProgressTitle(const QString&)),   &lcdHandler,    SLOT(on_setProgressTitle(const QString&)));
 
-    QObject::connect(&window,  SIGNAL(setGameInstallPath(const QString)), &updater,       SLOT(on_setGameInstallPath(const QString)));
+    QObject::connect(&window,  SIGNAL(setGameInstallPath(const QString&)), &updater,       SLOT(on_setGameInstallPath(const QString&)));
 
     window.show();
 
@@ -40,12 +40,12 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
-static void updaterWorkerThreadFunc(D2BlockUpdater* updater)
+static void updaterWorkerThreadFunc(D2BlockUpdater* const updater)
 {
 	updater->UpdateIgnoreList();
 }
 
-static void lcdHandlerWorkerThreadFunc(D2BlockLogitechLCD* lcdHandler)
+static void lcdHandlerWorkerThreadFunc(D2BlockLogitechLCD* const lcdHandler)
 {
 	lcdHandler->HandleLogitechLCD();
 }
